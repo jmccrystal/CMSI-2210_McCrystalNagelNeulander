d912f616c58cc3211b93c77c4bfa5307a5fc5123
Adam <adamneula@gmail.com>
2025-12-03 12:51:09 -0800

    find gcd in assembly and successfully call it in C

--- Homework/homework06/findGCD.s
+++ Homework/homework06/findGCD.s
@@ -1,40 +1,18 @@
-.global _main
-.global _gcd_func
+.global _findGCD
 .align 2
+.text
 
-.section __TEXT,__cstring
-_input_fmt:  .asciz "%ld %ld"
-_output_fmt: .asciz "GCD is: %ld\n"
-
-.section __TEXT,__text
-
-_main:
-    stp x29, x30, [sp, -16]!
-    ldr x0, =3113041662
-    ldr x1, =111570925
-    
-    bl _gcd_func
-
-    mov x1, x0
-    adrp x0, _output_fmt@PAGE
-    add  x0, x0, _output_fmt@PAGEOFF 
-    bl _printf
-    
-    mov x0, #0
-    ldp x29, x30, [sp], 16
-    ret
-
-_gcd_func:
+_findGCD:
     cmp x0, x1
     b.eq L_done
     b.lt L_swap
 
     sub x0, x0, x1
-    b _gcd_func
+    b _findGCD
 
 L_swap:
     sub x1, x1, x0
-    b _gcd_func
+    b _findGCD
 
 L_done:
     ret
---
+++ Homework/homework06/gcdFinder.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <assert.h>
+
+extern long findGCD(long a, long b);
+
+int main() {
+
+    long num1, num2;
+    
+    printf("Enter two numbers to find their GCD: ");
+    if (scanf("%ld %ld", &num1, &num2) == 2) {
+        long result = findGCD(num1, num2);
+        printf("GCD of %ld and %ld is: %ld\n", num1, num2, result);
+    }
+
+    return 0;
+}
